a8a277fa3520fc8daad1878da39ed6e53a6d4aca
Oleksii Butrin <bootreen@gmail.com>
2024-07-21 16:20:07 +0200

    Initial commit

--- server.js
+++ server.js
@@ -5,49 +5,52 @@ const { sql } = require("@vercel/postgres");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello from the Notes App - This is the demo" });
+app.get("/", (_, res) => {
+  res.json({ msg: "Songs DB for the Radio Bootreen" });
 });
 
-app.get("/notes", async (req, res) => {
-  const { rows } = await sql`SELECT * FROM notes`;
+app.get("/tracks", async (_, res) => {
+  const { rows } = await sql`SELECT * FROM tracks`;
   res.json(rows);
 });
 
-app.get("/notes/:id", async (req, res) => {
+app.get("/tracks/:id", async (req, res) => {
   const { id } = req.params;
-  const { rows } = await sql`SELECT * FROM notes WHERE notes.id = ${id} `;
+  const { rows } = await sql`SELECT * FROM tracks WHERE tracks.id = ${id} `;
   res.json(rows);
 });
 
-app.post("/notes", async (req, res) => {
-  const newNote = req.body;
+app.post("/tracks", async (req, res) => {
+  const { trackId, name, albumId, artistIds } = req.body;
+
+  // console.log(name);
 
   const { rowCount } =
-    await sql`INSERT INTO  notes (CONTENT, CATEGORY) VALUES (${newNote.content},${newNote.category})`;
+    await sql`INSERT INTO tracks (TRACK_ID, NAME, ALBUM_ID, ARTIST_IDS) VALUES
+      (${trackId},${name},${albumId},${artistIds})`;
 
   res.json({
-    msg: `A new element with content = ${newNote.content} and category= ${newNote.category} was added.`,
+    msg: `A new track ${name} was added, ${rowCount} tracks total.`,
   });
 });
 
-app.delete("/notes/:id", async (req, res) => {
-  const { id } = req.params;
+// app.delete("/notes/:id", async (req, res) => {
+//   const { id } = req.params;
 
-  const { rowCount } = await sql`DELETE FROM notes where id = ${id}`;
+//   const { rowCount } = await sql`DELETE FROM notes where id = ${id}`;
 
-  res.json({ msg: `Element with id=${id} successfully deleted` });
-});
+//   res.json({ msg: `Element with id=${id} successfully deleted` });
+// });
 
-app.patch("/notes/:id", async (req, res) => {
-  const { id } = req.params;
-  const { content, category } = req.body;
+// app.patch("/notes/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { content, category } = req.body;
 
-  const { rowCount } =
-    await sql`UPDATE notes SET content = ${content},category = ${category} WHERE id = ${id}`;
+//   const { rowCount } =
+//     await sql`UPDATE notes SET content = ${content},category = ${category} WHERE id = ${id}`;
 
-  res.json({ msg: `Element with id=${id} successfully updated` });
-});
+//   res.json({ msg: `Element with id=${id} successfully updated` });
+// });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
